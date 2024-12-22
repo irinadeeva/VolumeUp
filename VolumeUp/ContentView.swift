bc13645236564aec37df778c898f90e390c0711f
irinadeeva <deeva97@me.com>
2024-12-22 16:44:44 +0300

    Initial Commit

---
+++ VolumeUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VolumeUp
+//
+//  Created by Irina Deeva on 22/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VolumeUp/VolumeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeUpApp.swift
+//  VolumeUp
+//
+//  Created by Irina Deeva on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VolumeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
