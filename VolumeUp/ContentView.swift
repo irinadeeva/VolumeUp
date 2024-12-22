6471902b451b4d1dc3126d9337397abdd93c3f0c
irinadeeva <deeva97@me.com>
2024-12-22 18:29:06 +0300

    done

--- VolumeUp/ContentView.swift
+++ VolumeUp/ContentView.swift
@@ -1,24 +1,74 @@
-//
-//  ContentView.swift
-//  VolumeUp
-//
-//  Created by Irina Deeva on 22/12/24.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct CustomSlider: View {
+  @State private var sliderProgress: CGFloat = .zero
+  @State private var dragOffset: CGFloat = .zero
+  @State private var lastDragOffset: CGFloat = .zero
+  private let height = 150.0
+  private let width = 60.0
+
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      Rectangle()
+        .fill(.ultraThinMaterial)
+
+      Rectangle()
+        .fill(.white)
+        .frame(height: height * sliderProgress)
     }
+    .clipShape(.rect(cornerRadius: 15))
+    .optionalSizingModifier(
+      progress: sliderProgress,
+      height: height
+    )
+    .gesture(
+      DragGesture(minimumDistance: 0)
+        .onChanged {
+          let movement = -$0.translation.height + lastDragOffset
+          dragOffset = movement
+          calculateProgress(value: height)
+        }
+        .onEnded { _ in
+          withAnimation(.smooth) {
+            dragOffset = dragOffset > height ? height : (dragOffset < 0 ? 0 : dragOffset)
+            calculateProgress(value: height)
+          }
+
+          lastDragOffset = dragOffset
+        }
+    )
+    .frame(width: width, height: height, alignment: (sliderProgress < 0 ? .top : .bottom))
+  }
+
+  private func calculateProgress(value: CGFloat) {
+    let topAndTrailingExcessOffset = height + (dragOffset - height) * 0.1
+    let bottomAndLeadingExcessOffset = dragOffset < 0 ? dragOffset * 0.1 : dragOffset
+
+    let progress = (dragOffset > height ? topAndTrailingExcessOffset : bottomAndLeadingExcessOffset) / height
+    self.sliderProgress = progress
+  }
+
+}
+
+struct ContentView: View {
+
+  var body: some View {
+    VStack {
+      CustomSlider()
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(.blue)
+  }
+}
+
+fileprivate extension View {
+  @ViewBuilder
+  func optionalSizingModifier(progress: CGFloat, height: CGFloat) -> some View {
+    self
+      .frame(height: progress < 0 ? height + (-progress * height) : nil)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
